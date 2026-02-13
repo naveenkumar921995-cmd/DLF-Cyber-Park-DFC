0f1e6cb2d46c344db77a4b81893990a9e0ecfd4f
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-13 23:24:40 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -4,11 +4,10 @@ import os
 DB_PATH = "data/facility.db"
 
 def get_connection():
-    # Ensure data folder exists
     os.makedirs("data", exist_ok=True)
-    
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     return conn
+
 
 def init_db():
     conn = get_connection()
